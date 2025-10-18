db880fafdaa5647c6c8c06c4bc66d8bd85a83d8f
Ryan Brinton <brinton.school@gmail.com>
2025-10-17 23:23:14 -0600

    Only show splash screen video if the user is not logged in

--- lib/widgets/app_splash_wrapper.dart
+++ lib/widgets/app_splash_wrapper.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rate_my_bowl/screens/splash_screen.dart';
 import 'package:rate_my_bowl/widgets/auth_wrapper.dart';
+import 'package:rate_my_bowl/services/auth_service.dart';
 
 class AppSplashWrapper extends StatefulWidget {
   const AppSplashWrapper({super.key});
@@ -14,16 +16,28 @@ class _AppSplashWrapperState extends State<AppSplashWrapper> {
 
   @override
   Widget build(BuildContext context) {
-    if (_showSplash) {
-      return SplashScreen(
-        onAnimationComplete: () {
-          setState(() {
-            _showSplash = false;
+    return Consumer<AuthService>(
+      builder: (context, authService, child) {
+        if (authService.isAuthenticated && _showSplash) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            setState(() {
+              _showSplash = false;
+            });
           });
-        },
-      );
-    }
-    
-    return const AuthWrapper();
+        }
+
+        if (_showSplash) {
+          return SplashScreen(
+            onAnimationComplete: () {
+              setState(() {
+                _showSplash = false;
+              });
+            },
+          );
+        }
+        
+        return const AuthWrapper();
+      },
+    );
   }
 }
