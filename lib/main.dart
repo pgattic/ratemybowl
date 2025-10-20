b9182a46004b11d6b46ed51d5a00258d3fb7cf76
alveyE <gcbdalvey@gmail.com>
2025-10-20 16:17:32 -0600

    supabase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rate_my_bowl/services/auth_service.dart';
 import 'package:rate_my_bowl/widgets/app_splash_wrapper.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+
+void main() async {
+
+  await Supabase.initialize(
+    url: 'YOUR_SUPABASE_URL',
+    anonKey: 'YOUR_SUPABASE_PUBLISHABLE_KEY',
+  );
+  
   runApp(const MyApp());
 }
 
