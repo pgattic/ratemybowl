0f7b83efbee5885a06e9cb1d9fcfbe30455d65b5
Keaton Folsom <89952496+folskeat@users.noreply.github.com>
2025-10-26 18:11:54 -0600

    adjusted animation sliding distance

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -43,7 +43,7 @@ class _SplashScreenState extends State<SplashScreen>
     _slideAnimation =
         Tween<Offset>(
           begin: Offset.zero,
-          end: const Offset(0, -2.385), // move up 25% of its height
+          end: const Offset(0, -2.45), // up
         ).animate(
           CurvedAnimation(parent: _logoController, curve: Curves.easeInOut),
         );
