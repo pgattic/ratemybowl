8d260f150ead8b0fb78de965ddbdfb5c57db7fbe
Ryan Brinton <brinton.school@gmail.com>
2025-11-14 20:43:03 -0700

    Removed unused imports

--- lib/screens/adding_bathroom_screen.dart
+++ lib/screens/adding_bathroom_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import '../models/bathroom_location.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AddingBathroomScreen extends StatefulWidget {
